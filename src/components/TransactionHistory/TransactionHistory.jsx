82c39097e6f72984be8c75bc189b83503d365ff0
Valentina-K <kvalentyna@hotmail.com>
2022-12-29 20:23:13 +0200

    2

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -20,11 +20,12 @@ export const TransactionHistory = ({items}) => {
 }
 
 TransactionHistory.propTypes = {
-  items: PropTypes.exact({
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
     id: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     amount: PropTypes.string.isRequired,
     currency:PropTypes.string.isRequired,
-  })
-  
+    })
+  )  
 }
